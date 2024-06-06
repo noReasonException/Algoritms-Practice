c50fad441a1e908b79937cf59fd324052df9cd6e
Stefanos Stefanou <stefanos.stefanou@century.tech>
2024-06-06 15:49:58 +0100

    stash

---
+++ Problems/Set-1/8/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <math.h>
+#define MAXA pow(10,18)
+#define MAXL pow(2,8*8)
+
+using namespace std;
+
+void solve(){
+	cout<<(MAXA<MAXL);
+}
+
+int main(){
+	int n;
+	cin>>n;
+	while(n--)solve();
+}
