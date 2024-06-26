c29d6d178c7456f11f30c12c678ab0aad26fd86f
Stefanos Stefanou <stefanos.stefanou@century.tech>
2024-06-26 10:02:45 +0100

    debug fix

--- Problems/Ad-Hoc/1/main.cpp
+++ Problems/Ad-Hoc/1/main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 using namespace std;
 #ifdef DEBUG
-	#define debug cout
-	#define con <<
+	#define cinfo cout
+	#define add <<
 #endif
 #ifndef DEBUG
-	#define debug ;
-	#define con ;
+	#define cinfo ;
+	#define add ;
 #endif
 
 
 /**
- * 		Criterion
- * 			let array a1,a2,a3,a4 sorted, then its good if a4 = a1+a2+a3
- * 			let array a1,a2,a3,a4, then max(a1,a2,a3,a4) = others(a1,a2,a3,a4)
+ *	Criterion
+ *		let array a1,a2,a3,a4 sorted, then its good if a4 = a1+a2+a3
+ *		let array a1,a2,a3,a4, then max(a1,a2,a3,a4) = others(a1,a2,a3,a4)
 */
 int solve(){
-	int numbers=0,current=0,currentSum=0,max=0,indexes = 0;
+	long long numbers=0,current=0,currentSum=0,max=0,indexes=0;
 	cin>>numbers;
-	debug con "numbers\t" con numbers con "\n";
+	cinfo add "numbers\t" add numbers add "\n";
 	while(numbers--){
 		cin>>current;
 		if(current>max)max=current;
-		if(max==currentSum || max==currentSum+current) {
+		if(max==currentSum+current-max) {
 			indexes+=1;
-			debug con "\t\t\t\tindex is now" con indexes con "\n";
+			cinfo add "\t\t\t\tindex is now" add indexes add "\n";
 		}
-		debug con "number\t" con current con "\n";
-		debug con "\tmax\t" con max con "\n";
-		debug con "\tcurrent sum\t" con currentSum con "\n";
+		cinfo add "number\t" add current add "\n";
+		cinfo add "\tmax\t" add max add "\n";
+		cinfo add "\tcurrent sum\t" add currentSum add "\n";
 		currentSum+=current;
 
 	}
