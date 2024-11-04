fe25e9fea5214bd5e4359a942f44f705c2e7c32b
Stefanos Stefanou <stefanos.stefanou@century.tech>
2024-11-04 13:46:38 +0000

    feat: fixes

--- Problems/Informal/Sets/Set-1/7/main.cpp
+++ Problems/Informal/Sets/Set-1/7/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <sstream>
 #include <math.h>
+#include <set>
 #define ll long long
 #define ull unsigned long long
 #define ui unsigned int
@@ -23,7 +24,7 @@ template <typename container> void cinfoCon(container& genericSequence,string id
 		for(int i=0;i<depth;i++) prefix+="\t";
 
 		for(auto every: genericSequence){
-			cout<<prefix<<every<<"\n";
+			cout<<prefix<<"--->>"<<every<<"\n";
 		}
 		cout<<"=============cinfoCon Debug ("<<id<<") END =============\n";
 
@@ -38,35 +39,17 @@ template <typename container> void cinfoCon(container& genericSequence,string id
 
 #endif
 /***
- * The idea is simple and inefficient
- * 		if a candidate can fit 'exacly'
- * 			we split the string to segments of size k
- * 				every character of each segment should match  
- * 																		Seg-1    Seg-2 
- * 																	stormflame stornflame
- * 																	|		   |
- * 				has always offset = k.size()--->					------------
- * * 																stormflame stornflame
- * 																	 |		    |
- * 																	 ------------
- * 																		....
- * 																		....
- * * 																stormflame stornflame
- * 																	         |		    |
- * 																	          ------------
  */ 
-bool isOk(ui k_size,string &s){
-	ui confict = false;
-	ui s_size = s.size();
-	for(ui segment=1;segment<(s_size/k_size);segment+=1){
-		for(ui each_char=0;each_char<k_size;each_char+=1){
-			if(s[each_char] != s[(segment*k_size)+each_char]){
-				if(!confict) confict=true;
-				else return false;
+bool isOk(ui k_size,string &s,ui n){
+	ui confict = 0;
+	for(ui each_char=0;each_char<k_size;each_char++){
+		for(ui each_segment=1;each_segment<(n/k_size);each_segment+=2){
+			if(s[((each_segment-1)*k_size)+each_char] != s[(each_segment*k_size)+each_char]){
+				confict+=1;
 			}
 		}
 	}
-	return true;
+	return confict<2;
 }
 //A given candidate string k can only have a chance of being the minimum valid k if s.size() % k.size() == 0 (i.e 'fits exacly')
 void solve(){
@@ -77,7 +60,7 @@ void solve(){
 	for(ui i=1;i<=n;i++){
 		//does the current fit?
 		if(!(n%(i))){ //yeap
-			if(isOk(i,s)){
+			if(isOk(i,s,n)){
 				cout<<i;
 				return;
 			}
