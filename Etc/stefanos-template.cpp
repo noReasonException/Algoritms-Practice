8151215a968d671c1ce825ef0d5a4ebeff57d26f
Stefanos Stefanou <stefanos.stefanou@century.tech>
2024-11-21 07:37:20 +0000

    feat: Template Update. Stuff to look for + ios:sync_with_stdio trick

--- Etc/stefanos-template.cpp
+++ Etc/stefanos-template.cpp
@@ -33,18 +33,27 @@ template <typename container> void cinfoCon(container& genericSequence,string id
 
 
 /**
- *	Why?
+ 	*
+	? Stuff to look for ->
+    * stay organised
+    * int overflows, array bounds, etc.
+    * special cases (n=1)?
+    * do something instead of nothing
+    * timebox your approach
+    * simple is better than complex
+    * n % mod = (n % mod + mod) % mod; (Handling negatives)
+    
 */
 void solve(){
-	vector<int> in;
-	in.push_back(10);
-	in.push_back(20);
-	cinfoCon(in,"123",1);
-	in.push_back(20);
-	cinfoCon(in,"456",2);
+	return;
 }
 
 int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
 	int n;
 	cin>>n;
 	while(n--)solve();
